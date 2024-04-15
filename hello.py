da9354f5a07c6f3cbdea0d83df7ee78636db5882
Subash Gandyer <vejeysubash@gmail.com>
2024-04-15 12:18:27 -0400

    Make file

---
+++ hello.py
@@ -0,0 +1,7 @@
+def adder(a, b):
+    """
+    This function adds two numbers
+    """
+    return a + b
+
+print(adder(2,3))
---
+++ tests/test_hello.py
@@ -0,0 +1,4 @@
+from hello import adder
+
+def test_adder():
+    assert 8 == adder(3, 5)
